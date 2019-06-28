435cff84a3305b90d91d516716379ddc6bce12c5
shhuang123 <sharonhuang923@gmail.com>
2019-06-28 10:51:52 -0700

    revamp

--- src/App.js
+++ src/App.js
@@ -1,138 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from './components/Nav';
-import Vote from './components/Vote';
-import Chart from './components/Chart';
-import Form from './components/Form';
-import FirebaseApp from './components/firebase';
+//import chart from Chart.js
 
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props){
+    super(props)
     this.state = {
-      question: 'What is your favorite food?',
-      myOptions: [],
-      chartData: {},
-      showForm: false,
-    };
+      "burger": 0,
+      "salad": 0
+    }
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  toggleForm = () => {
+  handleClick(){
+    const choices = this.state + 1;
     this.setState({
-      showForm: ! this.state.showForm,
+      choices: choices
     });
+    console.log(this)
   }
 
-  componentDidMount () {
-    const list = [
-      {
-        id: 1,
-        myOption: 'Sushi',
-        votes: 0
-      },
-      {
-        id: 2,
-        myOption: 'Buger',
-        votes: 0
-      },
-      {
-        id: 3,
-        myOption: 'Hot Pot',
-        votes: 0
-      },
-      {
-        id: 4,
-        myOption: 'Salad',
-        votes: 0
-      },
-    ]
-    this.setState({
-      myOptions: list
-    });
-  }
+  //
+  // handleClick() {
+  //   this.setState({
+  //       choices: this.state.value + 1
+  //     });
+  //   console.log(this);
+  // }
 
-  componentWillMount() {
-    this.getChartData();
-  }
-
-  getChartData(){
-    this.setState({
-      chartData:{
-        labels: ['Sushi', 'Burger', 'Hot Pot', 'Salad'],
-        datasets: [
-          {
-            data: [25, 180, 35, 232],
-            backgroundColor: [
-              'rgba(255,99,132,0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(75, 206, 86, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(75, 206, 86, 1)',
-              'rgba(153, 102, 255, 1)',
-            ],
-            borderWidth: 1,
-            hoverBackgroundColor: [
-              'rgba(255,99,132,0.4)',
-              'rgba(54, 162, 235, 0.4)',
-              'rgba(75, 206, 86, 0.4)',
-              'rgba(153, 102, 255, 0.4)',
-            ]
-          }
-        ]
-      }
-    });
-  }
-
-  Vote = myOptionsId => {
-    const updatedList = this.state.myOptions.map(myOptions => {
-      if (myOptions.id === myOptionsId) {
-        return Object.assign({}, myOptions, {
-          votes: myOptions.votes + 1
-        });
-      }
-      else {
-        return myOptions;
-      }
-    });
-
-    this.setState({
-      myOptions: updatedList
-    });
-  };
-
-  render() {
-    const { showForm } = this.state;
-
-    return this.state.myOptions.map((myOptions) =>
-
-      <div className="App" key={myOptions.id}>
-      <Nav toggleForm={this.toggleForm} showForm={showForm} />
-
-      {showForm ?
-      <Form
-        submitForm={this.submitForm}
-      />
-      :
-      <Vote
-        question={this.state.question}
-        id={myOptions.id}
-        myOption={myOptions.myOption}
-        votes={myOptions.votes}
-        onVote={this.Vote}
-      />
-      }
-      <Chart
-        chartData={this.state.chartData}
-      />
-
+  render(){
+    const choices = this.state;
+    return (
+      <div className="display">
+        {Object.keys(choices).map((choice, i) => (
+          <div>
+            <button onClick={this.handleClick} key={choice} name={choice} value={choices}>{choice}</button>
+          </div>
+        ))}
       </div>
-    );
+    )
   }
+
 }
 
 export default App;
+
+// handleClick() {
+//   this.setState({
+//     burger: this.state.burger + 1,
+//     salad: this.state.salad + 1
+//   });
+//   console.log(this)
+// }
+
+// handleClick = () => {
+//   const {burger} = this.state
+//   this.setState({burger: burger + 1});
+// }
--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
 import * as firebase from 'firebase';
 
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBvaQ_VSykHV_4cFiz84-z7oJVpsKl_1yE",
     authDomain: "polls-6f51c.firebaseapp.com",
     databaseURL: "https://polls-6f51c.firebaseio.com",
-    projectId: "polls-6f51c",
     storageBucket: "gs://polls-6f51c.appspot.com/",
   };
 
-export const firebaseApp = firebase.initializeApp(firebase.config);
+export default firebaseConfig;
